0f599399f9eb66c449654c686494d22d6ed3945d
andru255 <andru255@gmail.com>
2017-07-11 18:46:12 -0500

    feat(docker): Adding support for windows environment

--- run-container.sh
+++ run-container.sh
@@ -1,3 +1,13 @@
 #!/bin/sh
 
-docker run --privileged --rm -e DEV_UID=$(id -u) -e DEV_GID=$(id -g) -v $(pwd):/usr/local/app andru255/docker-swift $*
+current_directory=$pwd
+
+#for windows + docker terminal
+command_exists (){
+    type "$1" &> /dev/null ;
+}
+if command_exists cygpath; then
+    current_directory=$(cygpath $PWD)
+fi
+echo "Running into: $current_directory"
+docker run --privileged --rm -e DEV_UID="$(id -u)" -e DEV_GID="$(id -g)" -v $current_directory:/usr/local/app/ andru255/docker-swift $*
